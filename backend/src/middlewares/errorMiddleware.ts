d4599362195922efeaced80e1d47165d927cbca9
Aditya <adithyap455@proton.me>
2025-11-21 10:06:33 +0530

    Created wrapper classes: ApiError, ApiResponse, and AsyncHandler for custom Error and Responses and handling Async functions. Added errorMiddleware to handle errors and return custom ApiError responses

---
+++ backend/src/middlewares/errorMiddleware.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
+import { ApiError } from "../utils/ApiError";
+
+export const errorMiddleware = (
+  err: ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal server error",
+    errors: err.errors || [],
+  });
+};
---
+++ backend/src/utils/ApiResponse.ts
@@ -0,0 +1,13 @@
+export class ApiResponse<T> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T | null;
+
+  constructor(statusCode: number, message: string, data: T | null = null) {
+    this.statusCode = statusCode;
+    this.success = statusCode < 400;
+    this.message = message;
+    this.data = data;
+  }
+}
