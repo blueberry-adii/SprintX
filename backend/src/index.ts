6bec84ef1a98b9ed58cdde9c215b887cd0bec1ae
Aditya <adithyap455@proton.me>
2025-11-21 10:43:44 +0530

    Added Dockerfiles in frontend and backend dir and docker-compose.yml in the root dir

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Backend Server running");
 });
 
-app.listen(3000, () => {
-  console.log("Server on http://localhost:3000");
+app.listen(5000, () => {
+  console.log("Server on http://localhost:5000");
 });
